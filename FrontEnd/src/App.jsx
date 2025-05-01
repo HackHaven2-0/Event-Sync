56dc96b3b346b7453e20c0de7b7c337d522e9cc5
Keshav1602 <keshav1602agarwal@gmail.com>
2025-05-02 00:37:45 +0530

    update

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './components/TempLandingPage';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/TempLandingPage";
+import "./App.css";
 // No changes needed as the import already matches the correct casing
 
 function App() {
   return (
     <div className="App">
-      
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* Add more routes as you build out your application */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/signup" element={<Signup />} /> */}
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        </Routes>
-      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Add more routes as you build out your application */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
     </div>
   );
 }
