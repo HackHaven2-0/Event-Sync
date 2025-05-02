4336c359969e730ead53ae983fd24f67d04c28ee
Keshav1602 <keshav1602agarwal@gmail.com>
2025-05-03 00:51:54 +0530

    fix: event card

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/TempLandingPage";
 import "./App.css";
+import EventCard from "../components/EventCard";
 // No changes needed as the import already matches the correct casing
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/card" element={<EventCard />} />
         {/* Add more routes as you build out your application */}
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/signup" element={<Signup />} /> */}
