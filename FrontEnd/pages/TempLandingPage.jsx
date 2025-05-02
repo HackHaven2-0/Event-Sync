f36cb258e86aa828a38ef6c11966643b5d9a83a2
garima <garima.7.2003@gmail.com>
2025-05-03 01:08:37 +0530

    final landing page

--- FrontEnd/pages/TempLandingPage.jsx
+++ FrontEnd/pages/TempLandingPage.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import Events from '../components/Events'; 
+
 // import HowItWorks from './HowItWorks';
 // import Testimonials from './Testimonials';
 // import CallToAction from './CallToAction';
-// import Footer from './Footer';
+import Footer from '../components/Footer';
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen w-full font-[Poppins] text-gray-800 bg-white overflow-x-hidden">
       <Navbar />
       <Hero />
-      <Features />
+      <Features id="features" />
+      <Events id="events" />
+
       {/* <HowItWorks />
       <Testimonials />
-      <CallToAction />
-      <Footer /> */}
+      <CallToAction /> */}
+      <Footer id="contact" />
     </div>
   );
 };
