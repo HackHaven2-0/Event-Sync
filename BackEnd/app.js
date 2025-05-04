95536d586cb0815ab2a66bb3066c525928b2d80a
Keshav1602 <keshav1602agarwal@gmail.com>
2025-05-04 10:41:11 +0530

    update

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,6 +7,7 @@ import userRouter from "./src/api/user/routes/userRoutes.js";
 import authRouter from "./src/api/auth/routes/authRoutes.js";
 import UserRouter from "./src/api/Users/routes/userRoutes.js";
 import EventRouter from "./src/api/event/routes/eventRoutes.js";
+import EmailRouter from "./src/api/email/routes/mailRouter.js";
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", UserRouter);
 app.use("/api/events", EventRouter);
+app.use("/api/email", EmailRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- BackEnd/src/api/email/routes/mailRouter.js
+++ BackEnd/src/api/email/routes/mailRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { authenticate } = require('../../Users/middlewares/authMiddleware.js');
 const { default: mailController } = require('../controllers/mailController.js');
 
-const router = express.Router();
+const EmailRouter = express.Router();
 
 // Define routes for email-related operations
-router.post('/send', authenticate ,mailController);
+EmailRouter.post('/send', authenticate ,mailController);
 
-module.exports = router;
+module.exports = EmailRouter;
